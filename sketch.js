a8060e758ecfd340208e22fab654debc4f385bbe
aryanaella <aryanaella@gmail.com>
2024-12-10 16:23:02 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
   // ANA MARIA DRAW AN AMAZING RECTANGLE
   fill('red');
   stroke('yellow');
-  rectangle(200,80,200,80);
+  rect(200,80,200,80);
 
   //ELLA DRAW A HUGE CIRCLE AND MAKE IT SQUASHED
   ellipse(500,500,100,400);
