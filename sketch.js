9696c45b4b861b702cbca197090f2516316e6502
nclufo <naomi.clough13@gmail.com>
2024-12-10 16:19:58 +0000

    draw fish

--- sketch.js
+++ sketch.js
@@ -13,4 +13,17 @@ function draw() {
   ellipse(500,500,100,400);
   
   //NAOMI DRAW A FISH 
+  fill(100, 200, 255);
+  noStroke();
+  ellipse(200, 200, 150, 80);
+
+
+  fill(100, 200, 255);
+  noStroke();
+  triangle(120, 200, 50, 160, 50, 240);
+
+  fill(0);
+  ellipse(230, 180, 15, 15);
+
+  // Aryana make the background change colour after 5 seconds
 }
