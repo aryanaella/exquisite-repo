d41ded9ff6186bf487790a1cd5890547d64beaff
Ana-Maria Margarit <anamariam2007@gmail.com>
2024-12-10 16:14:08 +0000

    FOR ELLLA

--- sketch.js
+++ sketch.js
@@ -5,4 +5,11 @@ function setup() {
 function draw() {
   background(220);
   // ANA MARIA DRAW AN AMAZING RECTANGLE
+  fill('red');
+  stroke('yellow');
+  rectangle(200,80,200,80);
+
+  //ELLA DRAW A HUGE CIRCLE AND MAKE IT SQUASHED
+  
+  
 }
