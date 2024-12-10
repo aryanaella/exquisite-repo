4c9db58fb9ec845556c4ad49272552793de96fd1
EllaWessendorff <ewessendorff@yahoo.com>
2024-12-10 16:23:07 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function draw() {
   rectangle(200,80,200,80);
 
   //ELLA DRAW A HUGE CIRCLE AND MAKE IT SQUASHED
-  ellipse(500,500,100,400);
+  ellipse(100,100,100,400);
   
   //NAOMI DRAW A FISH 
 }
